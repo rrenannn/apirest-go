9677ee63ee445f9065204535201819f7f2a54822
renan <renan.gamero1313@gmail.com>
2025-05-07 21:41:24 -0300

    usecase finalizada

---
+++ internal/usecase/product_usecase.go
@@ -0,0 +1,99 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"go-api/internal/db"
+	"strconv"
+	"time"
+	"strings"
+)
+
+type ProductUseCase struct {
+	repo *db.Queries
+}
+
+func NewProductUseCase(repo *db.Queries) *ProductUseCase {
+	return &ProductUseCase{repo: repo}
+}
+
+func validateCreateProduct(arg db.CreateProductParams) error {
+	if strings.TrimSpace(arg.Nome) == "" {
+        return errors.New("nome do produto é obrigatório")
+    }
+
+    if len(strings.TrimSpace(arg.Descricao)) < 10 {
+        return errors.New("descrição deve ter pelo menos 10 caracteres")
+    }
+
+    precoFloat, err := strconv.ParseFloat(arg.Preco, 64)
+    if err != nil {
+        return errors.New("preço inválido")
+    }
+
+    if precoFloat < 0 {
+        return errors.New("preço não pode ser negativo")
+    }
+
+    if arg.DataVenda.After(time.Now()) {
+        return errors.New("data de venda não pode ser futura")
+    }
+
+    return nil
+}
+
+func validateUpdateProduct(arg db.UpdateProductParams) error {
+	if strings.TrimSpace(arg.Nome) == "" {
+        return errors.New("nome do produto é obrigatório")
+    }
+
+    if len(strings.TrimSpace(arg.Descricao)) < 10 {
+        return errors.New("descrição deve ter pelo menos 10 caracteres")
+    }
+
+    precoFloat, err := strconv.ParseFloat(arg.Preco, 64)
+    if err != nil {
+        return errors.New("preço inválido")
+    }
+
+    if precoFloat < 0 {
+        return errors.New("preço não pode ser negativo")
+    }
+
+    if arg.DataVenda.After(time.Now()) {
+        return errors.New("data de venda não pode ser futura")
+    }
+
+    return nil
+}
+
+
+func (pu *ProductUseCase) GetProduct(ctx context.Context) ([]db.Product, error) {
+	return pu.repo.ListProduct(ctx)
+}
+
+func (pu *ProductUseCase) GetProductById(ctx context.Context, id int32) (db.Product, error) {
+	return pu.repo.GetProduct(ctx, id)
+}
+
+func (pu *ProductUseCase) CreateProduct(ctx context.Context, arg db.CreateProductParams) (db.Product, error) {
+	err := validateCreateProduct(arg)
+	if err != nil {
+		return db.Product{}, err
+	}
+
+	return pu.repo.CreateProduct(ctx, arg)
+}
+
+func (pu *ProductUseCase) UpdateProduct(ctx context.Context, arg db.UpdateProductParams) (db.Product, error) {
+	err := validateUpdateProduct(arg)
+	if err != nil {
+		return db.Product{}, err
+	}
+
+	return pu.repo.UpdateProduct(ctx, arg)
+}
+
+func (pu *ProductUseCase) DeleteProduct(ctx context.Context, id int32) error {
+	return pu.repo.DeleteProduct(ctx, id)
+}
